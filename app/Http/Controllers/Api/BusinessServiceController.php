ee42c5065d47f140bd9a63861b7ba1e667913ff3
migrain.code <migrain.code@gmail.com>
2023-10-30 23:53:09 +0300

    detail setup eklendi

--- app/Http/Controllers/Api/BusinessServiceController.php
+++ app/Http/Controllers/Api/BusinessServiceController.php
@@ -58,7 +58,7 @@ class BusinessServiceController extends Controller
     public function step2AddService(Request $request)
     {
         $business = $request->user();
-        $services = [];
+
         if ($request->type_id == "all"){
             $serviceSubCategory = ServiceSubCategory::find($request->input('sub_category'));
             $businessService = new BusinessService();
@@ -69,7 +69,7 @@ class BusinessServiceController extends Controller
             $businessService->time = $request->input('time');
             $businessService->price = $this->sayiDuzenle($request->input('price'));
             $businessService->save();
-            $services[]= $businessService;
+
             $serviceSubCategorys2 = ServiceSubCategory::where('slug', $serviceSubCategory->slug."-m")->first();
             $businessService = new BusinessService();
             $businessService->business_id = $business->id;
@@ -79,13 +79,19 @@ class BusinessServiceController extends Controller
             $businessService->time = $request->input('time');
             $businessService->price = $this->sayiDuzenle($request->input('price'));
             $businessService->save();
-            $services[]= $businessService;
 
-
+            $services = [
+                "id"=> $businessService->id,
+                "type"=> 'Kadın-Erkek',
+                "category"=> $businessService->categorys->name,
+                "sub_category"=> $businessService->subCategory->name,
+                "price"=> $businessService->price,
+                'time' => $businessService->time,
+            ];
             return response()->json([
                 'status' => "success",
                 'message' => "Yeni Hizmet Eklendi",
-                'businessServices' => BusinessServiceResource::collection($services),
+                'businessServices' => BusinessServiceResource::make($services),
             ]);
 
         }
@@ -99,12 +105,12 @@ class BusinessServiceController extends Controller
             $businessService->sub_category = $request->input('sub_category');
             $businessService->time = $request->input('time');
             $businessService->price = $this->sayiDuzenle($request->input('price'));
-            $services[]= $businessService;
+
             if ($businessService->save()) {
                 return response()->json([
                     'status' => "success",
                     'message' => "Yeni Hizmet Eklendi",
-                    'businessServices' => BusinessServiceResource::collection($services),
+                    'businessServices' => BusinessServiceResource::make($businessService),
                 ]);
             }
         }
