755927c7e9d91c795f1e5f514809d79c806a4d92
migrain.code <migrain.code@gmail.com>
2023-10-31 23:27:37 +0300

    api ile stripe ödeme test 1

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\OfficialPaymentRequest;
 use App\Http\Resources\BusinessPackageResource;
 //use App\Http\Resources\OfficialCardResource;
+use Stripe\Customer;
 use Stripe\EphemeralKey;
 use Stripe\Stripe;
 use Stripe\PaymentIntent;
@@ -34,23 +35,40 @@ class PaymentController extends Controller
         $business = $request->user();
         try {
             Stripe::setApiKey('sk_test_51NvSDhIHb2EidFuBWjFrNdghtNgToZOLbvopsjlNHfeiyNqw3hcZVNJo96iLJJXFhnJizZ5UXxVn8gLA7Kj268bI00vqpbTIOx');
+
+            // Stripe Müşteri oluşturun ve kullanıcı kimliğini (ID) ekleyin
+            $customer = Customer::create([
+                'metadata' => [
+                    'user_id' => $business->id,
+                ],
+            ]);
+
+            // Stripe Müşteri kimliği
+            $stripeCustomerId = $customer->id;
+
+            // Ephemeral Key'i oluşturun
             $ephemeralKey = EphemeralKey::create(
-                ['customer' => $business->id],
-                ['stripe_version' => '2023-10-16']
+                ['customer' => $stripeCustomerId],
+                ['stripe_version' => '2023-10-16'] // Stripe API sürümünü güncelleyin
             );
+
             $amount = 1000; // Ödeme miktarını ayarlayın (örnekte 10.00 dolar)
             $currency = 'EUR';
 
+            // PaymentIntent oluşturun ve metadata eklemeyi unutmayın
             $paymentIntent = PaymentIntent::create([
                 'amount' => $amount,
                 'currency' => $currency,
-                'description' => "Yeni Paket Alım Ödemesi"
+                'description' => "Yeni Paket Alım Ödemesi",
+                'metadata' => [
+                    'user_id' => $business->id,
+                ],
             ]);
 
             return response()->json([
                 'paymentIntent' => $paymentIntent->client_secret,
-                'ephemeralKey' => $ephemeralKey, // Ephemeral Key'i burada ayarlayın
-                'customer' => $business->id,
+                'ephemeralKey' => $ephemeralKey->secret,
+                'customer' => $stripeCustomerId,
             ]);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
