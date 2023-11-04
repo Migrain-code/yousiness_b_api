b4a0d527ee9a418e175ee7155dbc67a78fe8de04
migrain.code <migrain.code@gmail.com>
2023-11-04 19:49:52 +0300

    randevu api eklendi

--- app/Http/Resources/AppointmentDetailResoruce.php
+++ app/Http/Resources/AppointmentDetailResoruce.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 class AppointmentDetailResoruce extends JsonResource
 {
@@ -16,7 +17,7 @@ class AppointmentDetailResoruce extends JsonResource
     {
         return [
             'id' => $this->id,
-            'date' => $this->date->format('d.m.Y'),
+            'date' => Carbon::parse($this->date)->format('d.m.Y'),
             'status' => $this->status("text"),
             'comment_status' => $this->comment_status,
             'services' => AppointmentServiceResource::collection($this->services),
