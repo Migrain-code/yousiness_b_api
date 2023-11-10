d9669b989a31c061de1ea52f4c13cae5935e934d
migrain.code <migrain.code@gmail.com>
2023-11-10 20:33:41 +0300

    galller api eklendi

--- app/Http/Resources/BusinessSupportResource.php
+++ app/Http/Resources/BusinessSupportResource.php
@@ -21,6 +21,7 @@ class BusinessSupportResource extends JsonResource
           'status' => $this->status,
           'status_text' => $this->status == 0 ? "Cevaplanmadı" : "Cevaplandı",
           'answer' => $this->answer,
+          'created_at' => $this->created_at->format('d.m.Y H:i:s'),
         ];
     }
 }
