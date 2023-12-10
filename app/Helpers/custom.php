bdb8bf4ea38b2165ebd1dbb2a81848e9dbbcf814
migrain.code <migrain.code@gmail.com>
2023-12-10 20:28:08 +0300

    validation.php diller eklendi

--- app/Helpers/custom.php
+++ app/Helpers/custom.php
@@ -31,7 +31,7 @@ function clearPhone($phoneNumber) {
     if (in_array($inFormatPhone, $countryCodes)) {
         $phoneNumber = substr($phoneNumber, 4);
     }
-    $phoneNumber = preg_replace('/[^0-9+]/', '', $phoneNumber);
+    /*$phoneNumber = preg_replace('/[^0-9+]/', '', $phoneNumber);
 
     // Başındaki + işaretini ve ülke kodunu kontrol et
     if (substr($phoneNumber, 0, 1) == '+') {
@@ -46,7 +46,7 @@ function clearPhone($phoneNumber) {
     $phoneNumber = str_replace(' ', '', $phoneNumber);
 
     // Başındaki sıfırları kaldır
-    $phoneNumber = ltrim($phoneNumber, '0');
+    $phoneNumber = ltrim($phoneNumber, '0');*/
 
     return $phoneNumber;
 }
