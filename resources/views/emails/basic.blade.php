a5ba9e74ecaaa93fb0dfd364010e1be52ae542da
migrain.code <migrain.code@gmail.com>
2023-12-22 03:29:39 +0300

    mail test eklendi

--- resources/views/emails/basic.blade.php
+++ resources/views/emails/basic.blade.php
@@ -21,7 +21,7 @@
         <tr>
             <td align="center" valign="center" style="text-align:center; padding: 40px">
                 <a href="" rel="noopener" target="_blank">
-                    <img alt="Logo" style="width: 250px;" src="{{image(setting('bussiness_mail_logo'))}}" />
+                    <img alt="Logo" style="width: 250px;" src="https://business.yousiness.com/storage/settings/WNKiydGQYdeZOSVWBS9XaIhibOeuHsENQC0y8F8v.png" />
                 </a>
             </td>
         </tr>
